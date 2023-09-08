fab0e8b02044d9d0478db7acf001127e4dbf4185
Nana <neamat.gamal1@gmail.com>
2023-09-08 08:45:45 -0700

    it is going to be challanging

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**print reverse putchar using recursion**/
+void _print_rev_recursion(char *s);
+{
+	if (*s>'\0')
+	{
+		_print_rev_recursion(s+1);
+	       _putchar(*s);
+	}
+}
