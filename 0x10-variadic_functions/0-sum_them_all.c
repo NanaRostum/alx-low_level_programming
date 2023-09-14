4262d7d8e5f7eb54acfa40b35d70cf79e497f3ce
Nana <neamat.gamal1@gmail.com>
2023-09-14 00:57:05 -0700

    0. Beauty is variable, ugliness is constant

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum variable argument
+ * @n: the argument number
+ * @...: the integers sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int s = 0, i = n;
+	va_list a;
+
+	if (!n)
+		return (0);
+	va_start(ap, n);
+	while (i--)
+		s += va_arg(ap, int);
+	va_end(ap);
+	return (s);
+}
